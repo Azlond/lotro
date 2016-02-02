f8b6bc385a46f6c36b708230c429422f239a211d
bschattenberg <bschattenberg@BASC-Win7.athos.com>
2016-02-02 16:03:33 +0100

    switched to generated fxml controller class with empty handler methods created

--- RobotTest/src/gui/Start.java
+++ RobotTest/src/gui/Start.java
@@ -1,6 +1,9 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Start extends Application {
@@ -11,7 +14,12 @@ public class Start extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		StageFactory.getMainStage(primaryStage);
+		BorderPane root = (BorderPane) FXMLLoader.load(Start.class.getResource("/view/Start.fxml"));
+		
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Lotro Master");
+		primaryStage.sizeToScene();
 		primaryStage.show();
 	}
 	
